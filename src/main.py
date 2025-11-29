b37f581b99adc08478ea21e33bf394d463b902f5
cstorres-rep <cstorres.joao@gmail.com>
2025-11-29 01:32:03 -0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,30 @@
+#%%
+from src.processamento_de_dados import processamento
+from src.treinamento import treinamento
+from src.curva_roc import curva_roc
+
+def run_pipeline():
+
+    print(" Carregando e processando dados")
+    # Função processamento
+    df_analise = processamento('data/dataset_churn.xlsx')
+
+    print("\nSeparando caracteristicas (X) e churn target (y)...")
+    # 3. Separa as caracteristicas (X) e the churn target (y)
+    X = df_analise.drop(columns=['churn'])
+    y = df_analise['churn']
+
+    print("Treinando modelos ")
+    # 4. Call the treinamento function
+    df_predict = treinamento(X, y)
+
+   
+
+    print("\nMontando gráfico")
+    # 5. Plotando curva ROC
+    curva_roc(df_predict)
+    print("\Fim.")
+
+
+if __name__ == '__main__':
+    run_pipeline()
